6d7054d5846b01105957b93b9122eb3b30098502
Dan <dpedersen84@gmail.com>
2019-08-10 23:07:46 -0500

    http status

--- src/main/java/com/dp/supps/controllers/CategoryController.java
+++ src/main/java/com/dp/supps/controllers/CategoryController.java
@@ -4,11 +4,13 @@ import com.dp.supps.entities.Category;
 import com.dp.supps.service.CategoryService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,7 +33,8 @@ public class CategoryController {
         return categoryServ.getCategoryById(id);
     }
     
-    @PostMapping("/api/category/")
+    @PostMapping("/api/category")
+    @ResponseStatus(HttpStatus.CREATED)
     public Category addCategory(@RequestBody Category category) {
         return categoryServ.addCategory(category);
     }
